a4c4cd1acf1b85e72b9ab7a8284607549a9b6ad2
koba4real <kharefokba0@gmail.com>
2025-08-25 17:22:47 +0200

    completed table view for teams

--- TemaEsame20012023/resources/views/classifica.blade.php
+++ TemaEsame20012023/resources/views/classifica.blade.php
@@ -3,9 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Classifica Torneo</title>
 </head>
 <body>
     <h1>Classifica Torneo</h1>
+
+     <table class="table table-striped table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th>Squadra</th>
+                    <th>Punti</th>
+                    <th>PG</th>
+                    <th>V</th>
+                    <th>N</th>
+                    <th>P</th>
+                </tr>
+            </thead>
+        <tbody >
+             @isset($squadre)
+                    @forelse ($squadre as $squadra)
+                       <tr class="article-row" data-id="{{ $squadra->id }}" style="cursor: pointer;">
+                            <td>{{ $squadra->nome }}</td>
+                            <td><strong>{{ $squadra->punti }}</strong></td>
+                            <td>{{ $squadra->partite_giocate }}</td>
+                            <td>{{ $squadra->vittorie }}</td>
+                            <td>{{ $squadra->pareggi }}</td>
+                            <td>{{ $squadra->sconfitte }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="6" class="text-center">Nessuna squadra nel database. Eseguire il seeder.</td>
+                        </tr>
+                    @endforelse
+                @endisset
+        </tbody>
+    </table>
 </body>
 </html>
