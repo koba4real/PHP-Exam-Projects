2845650d315ab5399fb1729c105bcd3beb67db1f
koba4real <kharefokba0@gmail.com>
2025-08-28 14:09:32 +0200

    setup  TemaEsame28032024

---
+++ TemaEsame28032024/app/Http/Controllers/LucidiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\lucidi;
+use App\Http\Requests\StorelucidiRequest;
+use App\Http\Requests\UpdatelucidiRequest;
+
+class LucidiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorelucidiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(lucidi $lucidi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(lucidi $lucidi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatelucidiRequest $request, lucidi $lucidi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(lucidi $lucidi)
+    {
+        //
+    }
+}
