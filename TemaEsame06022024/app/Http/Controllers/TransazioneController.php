95ce3a7960e2bdf420a3de6fcd780d9e089c4885
koba4real <kharefokba0@gmail.com>
2025-08-28 09:14:34 +0200

    setup TemaEsame06022024

---
+++ TemaEsame06022024/app/Http/Controllers/TransazioneController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transazione;
+use App\Http\Requests\StoreTransazioneRequest;
+use App\Http\Requests\UpdateTransazioneRequest;
+
+class TransazioneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTransazioneRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Transazione $transazione)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Transazione $transazione)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTransazioneRequest $request, Transazione $transazione)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Transazione $transazione)
+    {
+        //
+    }
+}
